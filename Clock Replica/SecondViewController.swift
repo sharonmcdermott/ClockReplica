a17028256357633a9d0d1945b8bffc77dc6fa7fe
Sharon's Mac <help@pencilevent.com>
2016-07-27 08:08:38 -0400

    Initial Commit

---
+++ Clock Replica/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Clock Replica
+//
+//  Created by Sharon's Mac on 7/27/16.
+//  Copyright © 2016 Sharon McDermott. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
